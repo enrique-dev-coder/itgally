345274ae39ebfbf56434650d67e07fb6e85546cc
enrique95 <enrique_alvarado95@hotmail.com>
2022-04-18 11:04:32 -0500

    arrelgar errores de compilado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
