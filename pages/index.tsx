bbcd3725a70c21c4a9a6cf9dedf46896bd07f308
Eliman-C5 <eliman1926@gmail.com>
2022-04-18 12:09:57 -1200

    integracion de usuario

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>It gally prueba</title>
+        <title>It gally prueba y usando tailwind</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
