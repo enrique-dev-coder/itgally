7682eed6fe93594bc312c10537c5f7b753ed138f
eli castellano <eliman1926@gmail.com>
2022-06-22 15:17:28 -0400

    mostrando titlulo ITGALL

--- pages/projects/in4aha.jsx
+++ pages/projects/in4aha.jsx
@@ -3,37 +3,46 @@ import Contact from "../../sections/Contact"
 import SecondHeader from "../../components/atoms/SecondHeader"
 import BtnArrow from "../../components/atoms/BtnArrow"
 import FadeInElement from "../../components/FadeInElement"
+import { Fragment } from "react"
+import Head from 'next/head'
 
 const in4aha = () => {
     return (
-        <div className='xl:container xl:mx-auto'>
+        <Fragment>
+            <Head>
+                <title>IT GALL</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-            <SecondHeader imgURL="/assets/Group 22810.png" headerSize="h-[35vh] " pageLink='https://innovation4ageing.tehnopol.ee/' />
+            <div className='xl:container xl:mx-auto'>
 
-            <Navbar show={false}/>
+                <SecondHeader imgURL="/assets/Group 22810.png" headerSize="h-[35vh] " pageLink='https://innovation4ageing.tehnopol.ee/' />
+
+                <Navbar show={false}/>
 
 
-            <FadeInElement>
-                <div className="grid place-content-header px-[2rem] pt-[82px] pb-[3rem] sm:px-[4rem] lg:px-[11rem] lg:pb-[12px] xl:px-[16rem]">
+                <FadeInElement>
+                    <div className="grid place-content-header px-[2rem] pt-[82px] pb-[3rem] sm:px-[4rem] lg:px-[11rem] lg:pb-[12px] xl:px-[16rem]">
 
-                    <div className="flex flex-col">
+                        <div className="flex flex-col">
 
-                        <p className="text-primary-violet text-[1.2rem]">
+                            <p className="text-primary-violet text-[1.2rem]">
                             Innovation Networks for Active and Healthy Ageing (IN-4-AHA) is a EU-funded Coordination and Support Action (CSA) that contributes to the cross-border scale-up of tested and ready-to-use applications in health and social care. The project will bring together both the support and the demand sides as well as the end-users, engage with local and regional ecosystems, stakeholder groups and organisations. The main outcome of this cooperation is an innovation scale-up model that is validated by stakeholders and complemented by a clear implementation roadmap, an innovation impact evaluation toolkit and a strategy for long-term investments.
-                        </p>
+                            </p>
 
-                        <BtnArrow address="/projects/smartiago" aStyles="font-bold text-[#37044E] mt-[2rem] block text-[1.1rem] text-right" btnText="Next Project" />
+                            <BtnArrow address="/projects/smartiago" aStyles="font-bold text-[#37044E] mt-[2rem] block text-[1.1rem] text-right" btnText="Next Project" />
+                        </div>
+
                     </div>
+                </FadeInElement>
 
-                </div>
-            </FadeInElement>
-
-            <Contact
-                h3Styles="text-[#8C1D82] font-bold text-[1.3rem] mb-6"
-                pStyles="text-[#8C1D82] text-[1.2rem]"
-                aStyles="text-[#8C1D82] ml-[.5rem] text-[1.2rem] lg:text-[1rem]"
-            />
-        </div>
+                <Contact
+                    h3Styles="text-[#8C1D82] font-bold text-[1.3rem] mb-6"
+                    pStyles="text-[#8C1D82] text-[1.2rem]"
+                    aStyles="text-[#8C1D82] ml-[.5rem] text-[1.2rem] lg:text-[1rem]"
+                />
+            </div>
+        </Fragment>
     )
 }
 
--- pages/projects/smartiago.jsx
+++ pages/projects/smartiago.jsx
@@ -4,54 +4,63 @@ import Contact from "../../sections/Contact"
 import SecondHeader from "../../components/atoms/SecondHeader"
 import Arrow from "../../components/atoms/Arrow"
 import FadeInElement from "../../components/FadeInElement"
+import { Fragment } from 'react'
+import Head from 'next/head'
 
 const smartiago = () => {
     return (
-        <div className='xl:container xl:mx-auto'>
+        <Fragment>
+            <Head>
+                <title>IT GALL</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-            <SecondHeader imgURL="/assets/smartiago-grande.png" headerSize="h-[35vh] " pageLink='https://smartiago.santiagodecompostela.gal/es' />
+            <div className='xl:container xl:mx-auto'>
 
-            <Navbar show={false}/>
+                <SecondHeader imgURL="/assets/smartiago-grande.png" headerSize="h-[35vh] " pageLink='https://smartiago.santiagodecompostela.gal/es' />
+
+                <Navbar show={false}/>
 
 
-            <FadeInElement>
-                <div className="grid place-content-header px-[2rem] pt-[82px] pb-[3rem] sm:px-[4rem] lg:px-[11rem] lg:pb-[12px] xl:px-[16rem]">
+                <FadeInElement>
+                    <div className="grid place-content-header px-[2rem] pt-[82px] pb-[3rem] sm:px-[4rem] lg:px-[11rem] lg:pb-[12px] xl:px-[16rem]">
 
-                    <div className="flex flex-col">
+                        <div className="flex flex-col">
 
-                        <p className="text-primary-violet text-[1.2rem]">
-                            The living lab whose creation is promoted through a tender from <span className="font-bold">the City Hall of Santiago de Compostela</span>, will bring together an ecosystem of open innovation and technology transfer around advanced modeling, simulation and analysis systems that are developed, carrying out testing and networking actions that help incorporate R&amp;D&amp;I activities , in particular, these new technologies to the urban space to test and improve models of sustainable pedestrian mobility.
+                            <p className="text-primary-violet text-[1.2rem]">
+                                The living lab whose creation is promoted through a tender from <span className="font-bold">the City Hall of Santiago de Compostela</span>, will bring together an ecosystem of open innovation and technology transfer around advanced modeling, simulation and analysis systems that are developed, carrying out testing and networking actions that help incorporate R&amp;D&amp;I activities , in particular, these new technologies to the urban space to test and improve models of sustainable pedestrian mobility.
 
-                            <span className="font-bold block my-[2rem]">In this line, its specific objectives will be the following:</span>
+                                <span className="font-bold block my-[2rem]">In this line, its specific objectives will be the following:</span>
 
-                            <ul className="list-disc pl-[2rem]">
-                                <li className="mb-[.5rem]">Create a knowledge and learning space around modeling, simulation and personalized analysis of pedestrian mobility in urban environments through key agents in Santiago de Compostela, Galicia and Spain.</li>
-                                <li className="mb-[.5rem]">Create spaces (physical and virtual) for innovation that facilitate access to mobility modeling, simulation and analysis technologies for third parties interested in R&amp;D&amp;i processes related to sustainable mobility and, mainly, aimed at improving mobility. the environmental sustainability of pedestrian mobility habits in Heritage Cities or with characteristics similar to those of Santiago de Compostela.</li>
-                                <li className="mb-[.5rem]">Identify and attract the main trends, technologies, business models and solutions (hardware, software, solutions, etc.) existing at an international level around modeling, simulation and advanced analysis of mobility in urban environments.</li>
-                                <li className="mb-[.5rem]">Create connection spaces between the mobility industry, ICTs, citizens, local businesses or the public sector to promote entrepreneurship in the field of modeling, simulation and advanced analysis of urban mobility.</li>
-                                <li className="mb-[.5rem]">Promote the generation of employment and wealth in the area of ​​Santiago de Compostela by creating an innovation ecosystem that attracts talent and external investment and, at the same time, supports local talent and support/companies in areas such as technology or mobility.</li>
-                            </ul>
+                                <ul className="list-disc pl-[2rem]">
+                                    <li className="mb-[.5rem]">Create a knowledge and learning space around modeling, simulation and personalized analysis of pedestrian mobility in urban environments through key agents in Santiago de Compostela, Galicia and Spain.</li>
+                                    <li className="mb-[.5rem]">Create spaces (physical and virtual) for innovation that facilitate access to mobility modeling, simulation and analysis technologies for third parties interested in R&amp;D&amp;i processes related to sustainable mobility and, mainly, aimed at improving mobility. the environmental sustainability of pedestrian mobility habits in Heritage Cities or with characteristics similar to those of Santiago de Compostela.</li>
+                                    <li className="mb-[.5rem]">Identify and attract the main trends, technologies, business models and solutions (hardware, software, solutions, etc.) existing at an international level around modeling, simulation and advanced analysis of mobility in urban environments.</li>
+                                    <li className="mb-[.5rem]">Create connection spaces between the mobility industry, ICTs, citizens, local businesses or the public sector to promote entrepreneurship in the field of modeling, simulation and advanced analysis of urban mobility.</li>
+                                    <li className="mb-[.5rem]">Promote the generation of employment and wealth in the area of ​​Santiago de Compostela by creating an innovation ecosystem that attracts talent and external investment and, at the same time, supports local talent and support/companies in areas such as technology or mobility.</li>
+                                </ul>
 
-                        </p>
+                            </p>
 
-                        <Link href="/projects/in4aha">
-                            <a className="font-bold text-[#37044E] mt-[2rem] block text-[1.1rem]">
-                                <Arrow rotation="rotate(228deg)" mR="1rem" />
-                                Previous Project
-                            </a>
-                        </Link>
+                            <Link href="/projects/in4aha">
+                                <a className="font-bold text-[#37044E] mt-[2rem] block text-[1.1rem]">
+                                    <Arrow rotation="rotate(228deg)" mR="1rem" />
+                                    Previous Project
+                                </a>
+                            </Link>
+
+                        </div>
 
                     </div>
+                </FadeInElement>
 
-                </div>
-            </FadeInElement>
-
-            <Contact
-                h3Styles="text-[#8C1D82] font-bold text-[1.3rem] mb-6"
-                pStyles="text-[#8C1D82] text-[1.2rem]"
-                aStyles="text-[#8C1D82] ml-[.5rem] text-[1.2rem] lg:text-[1rem]"
-            />
-        </div>
+                <Contact
+                    h3Styles="text-[#8C1D82] font-bold text-[1.3rem] mb-6"
+                    pStyles="text-[#8C1D82] text-[1.2rem]"
+                    aStyles="text-[#8C1D82] ml-[.5rem] text-[1.2rem] lg:text-[1rem]"
+                />
+            </div>
+        </Fragment>
     )
 }
 
