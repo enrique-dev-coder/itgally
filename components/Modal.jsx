85306e746df44565361fa4a1195787b0dd066c34
eli castellano <eliman1926@gmail.com>
2022-07-14 17:22:29 -0400

    nuevo video en modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({modal, setModal}) => {
                     X
                 </button>
 
-                <video src="https://res.cloudinary.com/datsipxkz/video/upload/v1651768781/itgall/presentaci%C3%B3n_ITGALL_2_arkcwu.mp4" autoPlay controls loop></video>
+                <video src="https://res.cloudinary.com/datsipxkz/video/upload/v1657833441/itgall/presentacion-itgal-2-2_CmhRCNqg_s6bi0y.mp4" autoPlay controls loop></video>
             </div>
 
         </div>
